f9e7ac3ad9b8be010c12e33454722c59e2595f66
Septian Maulana <septianm028@gmail.com>
2023-03-23 11:43:31 +0700

    feat: create feature to show note by id

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { addNoteHandler, getAllNotesHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
 
 const routes = [
   {
@@ -10,6 +10,11 @@ const routes = [
     method: 'GET',
     path: '/notes',
     handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
   },
 ];
 
